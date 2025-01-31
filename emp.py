70f5a243adc6219de0012d4d0d0308b92b1277a9
Your Name <nikhilvardhanraju@gmail.com>
2025-01-31 14:37:48 +0530

    [Git] Add: python script to check attedance of an employee

--- emp.py
+++ emp.py
@@ -1,1 +1,13 @@
-print("Welcome to employee wage calcutation")
+import random
+
+def chk_attd():
+	print("Welcome to employee wage computation program on Main branch")
+	attendance = random.choice([0,1])
+	if attendance == 1:
+		print("The employee is present")
+	else:
+		print("The employee is absent")	
+
+if __name__ == "__main__":
+	chk_attd()
+	print("Welcome to employee wage computation")
