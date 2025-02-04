25548fa7cd803cafaca5673f93d392d0ce2f1489
Your Name <nikhilvardhanraju@gmail.com>
2025-02-04 10:38:33 +0530

    [Nikhil] Add: This python script to calculate daily wage of an employee

--- emp.py
+++ emp.py
@@ -7,12 +7,10 @@ def chk_attd():
 def cal_emp_daily_wage():
 	emp_check = chk_attd()
 	wage_per_hr = 20
-	if emp_check == 1 and random.random()>=0.5:
-		daily_wage = wage_per_hr * 8
-		print(f"The employee worked for full time and his daily wage is: {daily_wage}")
-	elif emp_check == 1 and random.random()<0.5:
-		daily_wage = wage_per_hr * 4
-		print(f"The employee worked for part time and his daily wage is: {daily_wage}")
+	full_day_hr = 8
+	if emp_check == 1:
+		daily_wage = wage_per_hr * full_day_hr
+		print(f"The employee is present and worked full time, his daily wage is: {daily_wage}")
 	else:
 		daily_wage = 0
 		print(f"The employee is absent and his daily wage is: {daily_wage}")
