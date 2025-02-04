5740c6cf907f64daa10ff55bdf84ad2378724390
Your Name <nikhilvardhanraju@gmail.com>
2025-02-04 11:37:26 +0530

    [Nikhil] Add: python scrip to calculate daily employee wage using switch case statement

--- emp.py
+++ emp.py
@@ -6,18 +6,25 @@ def chk_attd():
 
 def cal_emp_daily_wage():
 	emp_check = chk_attd()
+	if emp_check == 1:
+		work_type = random.choice(["full_time", "part_time"])
+	else:
+		work_type = "absent"
 	wage_per_hr = 20
 	full_day_hr = 8
 	part_time_hr = 4
-	if emp_check == 1 and random.random()>=0.5:
-		daily_wage = wage_per_hr * full_day_hr
-		print(f"The employee is present and worked full time, his daily wage is: {daily_wage}")
-	elif emp_check == 1 and random.random()<0.5:
-		daily_wage = wage_per_hr * part_time_hr
-		print(f"The employee is present and worked part time, his daily wage is: {daily_wage}")
-	else:
-		daily_wage = 0
-		print(f"The employee is absent and his daily wage is: {daily_wage}")
+ 
+	match work_type:
+		case "full_time":
+			daily_wage = wage_per_hr*full_day_hr
+			print(f"The employee is present and worked full time, his daily wage is: {daily_wage}")
+		case "part_time":
+			daily_wage = wage_per_hr*part_time_hr
+			print(f"The employee is present and worked part, his daily wage is: {daily_wage}")
+		case "absent":
+			daily_wage = 0
+			print(f"The employee is absent and his daily wage is: {daily_wage}")
+		
 
 if __name__ == "__main__":
 	cal_emp_daily_wage()
