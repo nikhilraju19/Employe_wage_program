fae80c3898bf0a2c7e24fd787a9fdb3c7160b7d6
Your Name <nikhilvardhanraju@gmail.com>
2025-02-16 14:12:25 +0530

    [Nikhil] Refactor: Maintained code hygiene.

--- emp.py
+++ emp.py
@@ -2,22 +2,21 @@ import random
 
 
 def chk_attd():
-	"""
+    """
 	Description:
 		This function is used to check the attendance of an employee
 	Parameters:
 		None
 	Return:
-		bool: True if the employee is present, False otherwise.
- 	"""
-	# Generate a random attendance status (1 for present, 0 for absent)
-	attendance = random.choice([0,1])
-	if attendance == 1:
-		print("The employee is present")
-		return True
-	else:
-		print("The employee is absent")	
-		return False
+		attendance
+    """
+    attendance = random.choice([0,1])
+    if attendance == 1:
+        print("The employee is present")
+        return True
+    else:
+        print("The employee is absent")	
+        return False
   
 def main():
     """
