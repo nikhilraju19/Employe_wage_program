60d3278a07631e8367e91e0b5f23621e73d7aa51
Your Name <nikhilvardhanraju@gmail.com>
2025-02-16 13:42:23 +0530

    [Nikhil] Refactor: Maintained code hygiene and refactored the code.

--- emp.py
+++ emp.py
@@ -2,38 +2,37 @@ import random
 
 
 def chk_attd():
-	"""
+    """
 	Description:
 		This function is used to check the attendance of an employee
 	Parameters:
 		None
 	Return:
 		attendance
- 	"""
-	# Generate a random attendance status (1 for present, 0 for absent)
-	attendance = random.choice([0,1])
-	return attendance
+    """
+    attendance = random.choice([0,1])
+    return attendance
 
 def cal_emp_daily_wage():
-	"""
-	Description:
+    """
+    Description:
 		This function is used to calculate the daily wage of an employee
 	Parameters:
 		None
 	Return:
 		The daily wage of the employee.
-	"""
-	emp_check = chk_attd()
-	wage_per_hr = 20
-	full_day_hr = 8
-	if emp_check == 1:
-		daily_wage = wage_per_hr * full_day_hr
-		print(f"The employee is present, his daily wage is: {daily_wage}")
-		return daily_wage
-	else:
-		daily_wage = 0
-		print(f"The employee is absent and his daily wage is: {daily_wage}")
-		return daily_wage
+    """
+    emp_check = chk_attd()
+    wage_per_hr = 20
+    full_day_hr = 8
+    if emp_check == 1:
+        daily_wage = wage_per_hr * full_day_hr
+        print(f"The employee is present, his daily wage is: {daily_wage}")
+        return daily_wage
+    else:
+        daily_wage = 0
+        print(f"The employee is absent and his daily wage is: {daily_wage}")
+        return daily_wage
   
 def main():
     """
