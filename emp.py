77ae10f4a01981438863953d923ebd41912564a3
Your Name <nikhilvardhanraju@gmail.com>
2025-02-16 14:22:44 +0530

    [Nikhil] Refactor: Maintained code hygiene and refactored the code.

--- emp.py
+++ emp.py
@@ -1,23 +1,55 @@
 import random
 
+
 def chk_attd():
-	attendance = random.choice([0,1])
-	return attendance
+    """
+	Description:
+		This function is used to check the attendance of an employee
+	Parameters:
+		None
+	Return:
+		attendance
+    """
+    attendance = random.choice([0,1])
+    return attendance
 
 def cal_emp_daily_wage():
-	emp_check = chk_attd()
-	wage_per_hr = 20
-	full_day_hr = 8
-	part_time_hr = 4
-	if emp_check == 1 and random.random()>=0.5:
-		daily_wage = wage_per_hr * full_day_hr
-		print(f"The employee is present and worked full time, his daily wage is: {daily_wage}")
-	elif emp_check == 1 and random.random()<0.5:
-		daily_wage = wage_per_hr * part_time_hr
-		print(f"The employee is present and worked part time, his daily wage is: {daily_wage}")
-	else:
-		daily_wage = 0
-		print(f"The employee is absent and his daily wage is: {daily_wage}")
+    """
+	Description:
+		This function is used to calculate the daily wage of an employee based on part time and full time
+	Parameters:
+		None
+	Return:
+		The daily wage of the employee
+    """
+    emp_check = chk_attd()
+    wage_per_hr = 20
+    full_day_hr = 8
+    part_time_hr = 4
+    if emp_check == 1 and random.random()>=0.5:
+        daily_wage = wage_per_hr * full_day_hr
+        print(f"The employee is present and worked full time, his daily wage is: {daily_wage}")
+        return daily_wage
+    elif emp_check == 1 and random.random()<0.5:
+        daily_wage = wage_per_hr * part_time_hr
+        print(f"The employee is present and worked part time, his daily wage is: {daily_wage}")
+        return daily_wage
+    else:
+        daily_wage = 0
+        print(f"The employee is absent and his daily wage is: {daily_wage}")
+        return daily_wage
+  
+def main():
+    """
+	Description:
+		This function calls all necessary sub-functions to perform the intended operations 
+   		of the program. It serves as the entry point when the script is run.
+	Parameters:
+		None
+	Return:
+		None
+    """
+    cal_emp_daily_wage()
 
 if __name__ == "__main__":
-	cal_emp_daily_wage()
+	main()
